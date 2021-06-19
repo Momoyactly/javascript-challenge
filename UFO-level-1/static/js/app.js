01c91acd1931f5f0dbd59012bee1ad1855826275
Uriel Arriaga <uriel.arriaga@momodevops.dev>
2021-06-19 18:11:20 -0500

    clean  level 1

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -8,10 +8,6 @@ var filterButton = d3.select("#filter-btn");
 
 function handleChange(event) {
     var date = d3.select("#datetime").property("value");
-    var city = d3.select("#city").property("value");
-    var state = d3.select("#state").property("value");
-    var country = d3.select("#country").property("value");
-    console.log(date,city,state,country);
     table.text("");
     if (date != 0) {
         data.filter(data => data.datetime === date).forEach(i => {
