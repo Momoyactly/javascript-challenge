0e83d47b644aabb77db213901776782885322a6d
Uriel Arriaga <uriel.arriaga@momodevops.dev>
2021-06-19 19:19:05 -0500

    added level 2 city,state,country

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -7,14 +7,19 @@ var table = d3.select("table>tbody");
 var filterButton = d3.select("#filter-btn");
 
 function handleChange(event) {
-    var date = d3.select("#datetime").property("value");
-    var city = d3.select("#city").property("value");
-    var state = d3.select("#state").property("value");
-    var country = d3.select("#country").property("value");
-    console.log(date,city,state,country);
+    values = {};
+    values['datetime'] = d3.select("#datetime").property("value");
+    values['city'] = d3.select("#city").property("value");
+    values['state'] = d3.select("#state").property("value");
+    values['country'] = d3.select("#country").property("value");
+    var filteredValues = Object.entries(values).filter(i =>i[1] != 0);
     table.text("");
-    if (date != 0) {
-        data.filter(data => data.datetime === date).forEach(i => {
+    if (filteredValues != 0) {
+        var filteredData = data;
+        filteredValues.forEach(value =>{
+            filteredData = filteredData.filter(i =>i[value[0]] === value[1] );
+        } );
+        filteredData.forEach(i => {
             var tr = table.append("tr");
             Object.values(i).forEach(j => {
                 tr.append("td").text(j);
